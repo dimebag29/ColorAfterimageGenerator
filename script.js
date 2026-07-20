68dafcaa9bb9282432c7989ebc46191afafcc516
dimebag29 <fused.deposition.modeling@gmail.com>
2026-07-20 19:20:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const fileInput = document.getElementById("fileInput");
 
 const originalCanvas = document.getElementById("originalCanvas");
+const canvasA = document.getElementById("canvasA");
+const canvasB = document.getElementById("canvasB");
+
 const originalCtx = originalCanvas.getContext("2d");
+const ctxA = canvasA.getContext("2d");
+const ctxB = canvasB.getContext("2d");
+
+let currentImage = null;
 
 fileInput.addEventListener("change", loadImage);
 
@@ -18,12 +25,34 @@ function loadImage(event)
 
     img.onload = function ()
     {
-        originalCanvas.width = img.width;
-        originalCanvas.height = img.height;
+        currentImage = img;
 
-        originalCtx.clearRect(0, 0, img.width, img.height);
-        originalCtx.drawImage(img, 0, 0);
+        drawOriginal();
+
+        updateImageA();
+
+        updateImageB();
     };
 
     img.src = URL.createObjectURL(file);
+}
+
+function drawOriginal()
+{
+    originalCanvas.width = currentImage.width;
+    originalCanvas.height = currentImage.height;
+
+    originalCtx.clearRect(0, 0, currentImage.width, currentImage.height);
+
+    originalCtx.drawImage(currentImage, 0, 0);
+}
+
+function updateImageA()
+{
+    // 後で作る
+}
+
+function updateImageB()
+{
+    // 後で作る
 }
