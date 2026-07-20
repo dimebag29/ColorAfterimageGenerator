1e36028ae620e6aa5d244897ce6b57c86357d689
dimebag29 <fused.deposition.modeling@gmail.com>
2026-07-20 19:15:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("script loaded");
+const fileInput = document.getElementById("fileInput");
+
+const originalCanvas = document.getElementById("originalCanvas");
+const originalCtx = originalCanvas.getContext("2d");
+
+fileInput.addEventListener("change", loadImage);
+
+function loadImage(event)
+{
+    const file = event.target.files[0];
+
+    if (!file)
+    {
+        return;
+    }
+
+    const img = new Image();
+
+    img.onload = function ()
+    {
+        originalCanvas.width = img.width;
+        originalCanvas.height = img.height;
+
+        originalCtx.clearRect(0, 0, img.width, img.height);
+        originalCtx.drawImage(img, 0, 0);
+    };
+
+    img.src = URL.createObjectURL(file);
+}
